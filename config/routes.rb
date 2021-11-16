dc532fdcf17fcb5088a6a287b3fe33702d31e991
Eric Yi <erickanginyi@gmail.com>
2021-11-16 16:13:46 -0500

    Comment out unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,17 @@ Rails.application.routes.draw do
   resources :contracts
   resources :lendees
   resources :lenders
+
+  get "/cookie_click", to: "sessions#click"
+
+  # # Login session
+  # post "/login", to: "sessions#create"
+  # # Login action
+  # get "/me", to: "users#show"
+
+
+
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
