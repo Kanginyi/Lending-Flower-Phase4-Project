af61fd89d69922c48d281ef2b440aacc74f41a2a
Eric Yi <erickanginyi@gmail.com>
2021-11-16 16:13:23 -0500

    Create login component

---
+++ client/src/Components/Login.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import GoldBackground from "../GoldBackground.mp4"
+
+function Login() {
+    return (
+        <div id="login-background">
+            <video
+                id="background-video"
+                src={GoldBackground}
+                loop
+                muted
+                autoPlay={true}
+            />
+            <h1 id="logo">Lending Breeze</h1>
+
+
+            <form id="form-flex">
+                <span className="login-text-bold">Username:</span>
+                <input type="text" name="username" /><br/>
+                <span className="login-text-bold">Password:</span>
+                <input type="password" name="password" />
+                <br/>
+                <button>Login</button><br/>
+                <button>Signup</button>
+            </form>
+        </div>
+    );
+}
+
+<div>
+<video
+    id="background-video"
+    src={GoldBackground}
+    loop
+    muted
+    autoPlay={true}
+/>
+<h1 id="logo">Lending Breeze</h1>
+</div>
+
+export default Login;
