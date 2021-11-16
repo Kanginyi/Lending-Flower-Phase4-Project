b70a89df1ad898490dd4d38d43828cf0bff29bcf
Eric Yi <erickanginyi@gmail.com>
2021-11-16 10:08:21 -0500

    Create components

---
+++ client/src/Components/ContractContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ContractContainer() {
+    return (
+        <h2>wtf</h2>
+    );
+}
+
+export default ContractContainer;
---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <h3>what</h3>
+    );
+}
+
+export default Navbar;
