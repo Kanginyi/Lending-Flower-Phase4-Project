22448deea71ab2a6ae556eae4c6c488e7ed23f90
Eric Yi <erickanginyi@gmail.com>
2021-11-17 15:30:46 -0500

    Add font and styling to Home

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -4,8 +4,8 @@ import GoldBackground from "../GoldBackground.mp4";
 function Home() {
   return (
     <div id="homepage">
-      <h1>Welcome to Lending Breeze</h1>
-      <h3>Initiating loans with ease</h3>
+      <h1 id="homepage-welcome">Lending Breeze</h1>
+      <h3 id="homepage-slogan">Initiating Loans With Ease</h3>
       <video
         id="background-video"
         src={GoldBackground}
@@ -13,16 +13,17 @@ function Home() {
         muted
         autoPlay={true}
       />
-      <p>
-        Lending Breeze is a lending platform where you can compare rates from
-        accredited lenders and find the best deals for your loans.
-      </p>
-      <p>
-        Whether you want to buy a house or a mouse, or you want to create a
-        company to sell your famous chili (don't drop it), we've got you
-        covered. Lending Breeze really makes it a breeze! We're the only online
-        loan marketplace that is fake af but also looks legit af.
-      </p>
+      <div id="homepage-paras-div">
+        <p className="homepage-paras">
+          Lending Breeze is a lending platform where you can compare rates from
+          accredited lenders and find the best deals for your loans.
+        </p>
+        <p className="homepage-paras">
+          Whether you want to buy a house or want to start a small business to sell your famous chili (don't drop it), we've got you
+          covered. Lending Breeze really makes it a breeze! We're the only online
+          loan marketplace that is fake af but also looks legit af.
+        </p>
+      </div>
     </div>
   );
 }
