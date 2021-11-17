f464dc76512f903cf2fe670d549c7d85f06afe23
Eric Yi <erickanginyi@gmail.com>
2021-11-16 23:13:14 -0500

    Add Routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import Navbar from "./Components/Navbar";
 import ContractContainer from "./Components/ContractContainer";
 import Login from "./Components/Login";
+import Lenders from "./Components/Lenders";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
 
 function App() {
   return (
     <>
+    <Router>
         <Navbar/>
-        <ContractContainer />
-        <Login />
-        <br/><br/><br/><br/><br/>
-     </>
+        <div id="main">
+        <Routes>
+          <Route path="lenders" element={ <Lenders />}/>
+          <Route path="contracts" element={ <ContractContainer />}/>
+          <Route path="login" element={ <Login /> }/>
+        </Routes>
+        </div>
+    </Router>
+    </>
   );
 }
 
