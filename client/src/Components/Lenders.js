6742a9fc0cd1eb854b18c8df30f40b430585bb43
+ <+>
2021-11-18 11:25:37 -0500

    loan descriptions

--- client/src/Components/Lenders.js
+++ client/src/Components/Lenders.js
@@ -1,55 +1,53 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
 function Lenders() {
-    const [lendersData, setLendersData] = useState([]);
+  const [lendersData, setLendersData] = useState([]);
 
-    useEffect(() => {
-        fetch("/lenders")
-            .then(resp => resp.json())
-            .then(data => {
-                if (data.length > 0) {
-                    setLendersData(data);
-                }
-            })
-    }, [])
+  useEffect(() => {
+    fetch("/lenders")
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (data.length > 0) {
+          setLendersData(data);
+        }
+      });
+  }, []);
 
-    const jpMorgan = lendersData[0];
-    const canadia = lendersData[1];
-    const capitalTwo = lendersData[2];
-    const silverman = lendersData[3];
-    const ttBank = lendersData[4];
-        
+  const jpMorgan = lendersData[0];
+  const canadia = lendersData[1];
+  const capitalTwo = lendersData[2];
+  const silverman = lendersData[3];
+  const ttBank = lendersData[4];
 
-    return (
-        <div>
-            <div className="lender-card">
-                <h3>{jpMorgan?.name}</h3>
-                <h4>Current Interest Rate: {jpMorgan?.interest_rate}%</h4>
-                <p>Description: asdf</p>
-            </div>
-            <div className="lender-card">
-                <h3>{canadia?.name}</h3>
-                <h4>Current Interest Rate: {canadia?.interest_rate}%</h4>
-                <p>Description: asdf</p>
-            </div>
-            <div className="lender-card">
-                <h3>{capitalTwo?.name}</h3>
-                <h4>Current Interest Rate: {capitalTwo?.interest_rate}%</h4>
-                <p>Description: asdf</p>
-            </div>
-            <div className="lender-card">
-                <h3>{silverman?.name}</h3>
-                <h4>Current Interest Rate: {silverman?.interest_rate}%</h4>
-                <p>Description: asdf</p>
-            </div>
-            <div className="lender-card">
-                <h3>{ttBank?.name}</h3>
-                <h4>Current Interest Rate: {ttBank?.interest_rate}%</h4>
-                <p>Description: asdf</p>
-            </div>
-        </div>
-
-    );
+  return (
+    <div>
+      <div className="lender-card">
+        <h3>{jpMorgan?.name}</h3>
+        <h4>Current Interest Rate: {jpMorgan?.interest_rate}%</h4>
+        <p>Description: Interest Only For First Three Months</p>
+      </div>
+      <div className="lender-card">
+        <h3>{canadia?.name}</h3>
+        <h4>Current Interest Rate: {canadia?.interest_rate}%</h4>
+        <p>Description: Debt Consolidation</p>
+      </div>
+      <div className="lender-card">
+        <h3>{capitalTwo?.name}</h3>
+        <h4>Current Interest Rate: {capitalTwo?.interest_rate}%</h4>
+        <p>Description: Fixed Rate Mortgage</p>
+      </div>
+      <div className="lender-card">
+        <h3>{silverman?.name}</h3>
+        <h4>Current Interest Rate: {silverman?.interest_rate}%</h4>
+        <p>Description: Student Loan</p>
+      </div>
+      <div className="lender-card">
+        <h3>{ttBank?.name}</h3>
+        <h4>Current Interest Rate: {ttBank?.interest_rate}%</h4>
+        <p>Description: Auto Loan</p>
+      </div>
+    </div>
+  );
 }
 
 export default Lenders;
--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
 function Navbar() {
-    return(
-        <>
-        <nav className="navbar">
+  return (
+    <>
+      <nav className="navbar">
         <ul className="navbar-nav">
-            <li className="navbar-logo">
+          <li className="navbar-logo">
             <a href="/" className="nav-link">
-                <span className="link-text logo-text">Lending</span>
-                <svg
+              <span className="link-text logo-text">Lending</span>
+              <svg
                 aria-hidden="true"
                 focusable="false"
                 data-prefix="fad"
@@ -17,26 +17,26 @@ function Navbar() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 448 512"
                 className="svg-inline--fa fa-angle-double-right fa-w-14 fa-5x"
-                >
+              >
                 <g className="fa-group">
-                    <path
+                  <path
                     fill="currentColor"
                     d="M224 273L88.37 409a23.78 23.78 0 0 1-33.8 0L32 386.36a23.94 23.94 0 0 1 0-33.89l96.13-96.37L32 159.73a23.94 23.94 0 0 1 0-33.89l22.44-22.79a23.78 23.78 0 0 1 33.8 0L223.88 239a23.94 23.94 0 0 1 .1 34z"
                     className="fa-secondary"
-                    ></path>
-                    <path
+                  ></path>
+                  <path
                     fill="currentColor"
                     d="M415.89 273L280.34 409a23.77 23.77 0 0 1-33.79 0L224 386.26a23.94 23.94 0 0 1 0-33.89L320.11 256l-96-96.47a23.94 23.94 0 0 1 0-33.89l22.52-22.59a23.77 23.77 0 0 1 33.79 0L416 239a24 24 0 0 1-.11 34z"
                     className="fa-primary"
-                    ></path>
+                  ></path>
                 </g>
-                </svg>
+              </svg>
             </a>
-            </li>
-    
-            <li className="nav-item">
+          </li>
+
+          <li className="nav-item">
             <a href="/login" className="nav-link">
-                <svg
+              <svg
                 aria-hidden="true"
                 focusable="false"
                 data-prefix="fas"
@@ -45,27 +45,27 @@ function Navbar() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 640 512"
                 className="svg-inline--fa fa-door-open fa-w-20"
-                >
+              >
                 <g className="fa-group">
-                <path
-                fill="currentColor"
-                d="M624 448h-80V113.45C544 86.19 522.47 64 496 64H384v64h96v384h144c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16zM312.24 1.01l-192 49.74C105.99 54.44 96 67.7 96 82.92V448H16c-8.84 0-16 7.16-16 16v32c0 8.84 7.16 16 16 16h336V33.18c0-21.58-19.56-37.41-39.76-32.17zM264 288c-13.25 0-24-14.33-24-32s10.75-32 24-32 24 14.33 24 32-10.75 32-24 32z"
-                className="fa-secondary"
-                ></path>
-                <path
+                  <path
+                    fill="currentColor"
+                    d="M624 448h-80V113.45C544 86.19 522.47 64 496 64H384v64h96v384h144c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16zM312.24 1.01l-192 49.74C105.99 54.44 96 67.7 96 82.92V448H16c-8.84 0-16 7.16-16 16v32c0 8.84 7.16 16 16 16h336V33.18c0-21.58-19.56-37.41-39.76-32.17zM264 288c-13.25 0-24-14.33-24-32s10.75-32 24-32 24 14.33 24 32-10.75 32-24 32z"
+                    className="fa-secondary"
+                  ></path>
+                  <path
                     fill="currentColor"
                     d="M376 144a16 16 0 1 0 16 16 16 16 0 0 0-16-16zm80 0a16 16 0 1 0 16 16 16 16 0 0 0-16-16zM131.06 273.53L192 304l-23.52-70.56a192.06 192.06 0 0 0-37.42 40.09zM256 272v-77.11a198.62 198.62 0 0 0-43.15 12.38z"
                     className="fa-primary"
-                ></path>
+                  ></path>
                 </g>
-                </svg>
-                <span className="link-text">Login</span>
+              </svg>
+              <span className="link-text">Login</span>
             </a>
-            </li>
-    
-            <li className="nav-item">
+          </li>
+
+          <li className="nav-item">
             <a href="/" className="nav-link">
-                <svg
+              <svg
                 aria-hidden="true"
                 focusable="false"
                 data-prefix="fas"
@@ -73,22 +73,23 @@ function Navbar() {
                 role="img"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 576 512"
-                className="svg-inline--fa fa-hand-holding-usd fa-w-18">
+                className="svg-inline--fa fa-hand-holding-usd fa-w-18"
+              >
                 <g className="fa-group">
-                <path
-                fill="currentColor"
-                d="M271.06,144.3l54.27,14.3a8.59,8.59,0,0,1,6.63,8.1c0,4.6-4.09,8.4-9.12,8.4h-35.6a30,30,0,0,1-11.19-2.2c-5.24-2.2-11.28-1.7-15.3,2l-19,17.5a11.68,11.68,0,0,0-2.25,2.66,11.42,11.42,0,0,0,3.88,15.74,83.77,83.77,0,0,0,34.51,11.5V240c0,8.8,7.83,16,17.37,16h17.37c9.55,0,17.38-7.2,17.38-16V222.4c32.93-3.6,57.84-31,53.5-63-3.15-23-22.46-41.3-46.56-47.7L282.68,97.4a8.59,8.59,0,0,1-6.63-8.1c0-4.6,4.09-8.4,9.12-8.4h35.6A30,30,0,0,1,332,83.1c5.23,2.2,11.28,1.7,15.3-2l19-17.5A11.31,11.31,0,0,0,368.47,61a11.43,11.43,0,0,0-3.84-15.78,83.82,83.82,0,0,0-34.52-11.5V16c0-8.8-7.82-16-17.37-16H295.37C285.82,0,278,7.2,278,16V33.6c-32.89,3.6-57.85,31-53.51,63C227.63,119.6,247,137.9,271.06,144.3ZM565.27,328.1c-11.8-10.7-30.2-10-42.6,0L430.27,402a63.64,63.64,0,0,1-40,14H272a16,16,0,0,1,0-32h78.29c15.9,0,30.71-10.9,33.25-26.6a31.2,31.2,0,0,0,.46-5.46A32,32,0,0,0,352,320H192a117.66,117.66,0,0,0-74.1,26.29L71.4,384H16A16,16,0,0,0,0,400v96a16,16,0,0,0,16,16H372.77a64,64,0,0,0,40-14L564,377a32,32,0,0,0,1.28-48.9Z"
-                className="fa-secondary"></path>
+                  <path
+                    fill="currentColor"
+                    d="M271.06,144.3l54.27,14.3a8.59,8.59,0,0,1,6.63,8.1c0,4.6-4.09,8.4-9.12,8.4h-35.6a30,30,0,0,1-11.19-2.2c-5.24-2.2-11.28-1.7-15.3,2l-19,17.5a11.68,11.68,0,0,0-2.25,2.66,11.42,11.42,0,0,0,3.88,15.74,83.77,83.77,0,0,0,34.51,11.5V240c0,8.8,7.83,16,17.37,16h17.37c9.55,0,17.38-7.2,17.38-16V222.4c32.93-3.6,57.84-31,53.5-63-3.15-23-22.46-41.3-46.56-47.7L282.68,97.4a8.59,8.59,0,0,1-6.63-8.1c0-4.6,4.09-8.4,9.12-8.4h35.6A30,30,0,0,1,332,83.1c5.23,2.2,11.28,1.7,15.3-2l19-17.5A11.31,11.31,0,0,0,368.47,61a11.43,11.43,0,0,0-3.84-15.78,83.82,83.82,0,0,0-34.52-11.5V16c0-8.8-7.82-16-17.37-16H295.37C285.82,0,278,7.2,278,16V33.6c-32.89,3.6-57.85,31-53.51,63C227.63,119.6,247,137.9,271.06,144.3ZM565.27,328.1c-11.8-10.7-30.2-10-42.6,0L430.27,402a63.64,63.64,0,0,1-40,14H272a16,16,0,0,1,0-32h78.29c15.9,0,30.71-10.9,33.25-26.6a31.2,31.2,0,0,0,.46-5.46A32,32,0,0,0,352,320H192a117.66,117.66,0,0,0-74.1,26.29L71.4,384H16A16,16,0,0,0,0,400v96a16,16,0,0,0,16,16H372.77a64,64,0,0,0,40-14L564,377a32,32,0,0,0,1.28-48.9Z"
+                    className="fa-secondary"
+                  ></path>
                 </g>
-                </svg>
-                <span className="link-text">New Loan</span>
+              </svg>
+              <span className="link-text">New Loan</span>
             </a>
-            </li>
- 
-    
-            <li className="nav-item">
+          </li>
+
+          <li className="nav-item">
             <a href="/lenders" className="nav-link">
-                <svg
+              <svg
                 aria-hidden="true"
                 focusable="false"
                 data-prefix="fas"
@@ -96,29 +97,28 @@ function Navbar() {
                 role="img"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
-                className="svg-inline--fa fa-university fa-w-16">
+                className="svg-inline--fa fa-university fa-w-16"
+              >
                 <g className="fa-group">
-                <path
-                fill="currentColor"
-                d="M496 128v16a8 8 0 0 1-8 8h-24v12c0 6.627-5.373 12-12 12H60c-6.627 0-12-5.373-12-12v-12H24a8 8 0 0 1-8-8v-16a8 8 0 0 1 4.941-7.392l232-88a7.996 7.996 0 0 1 6.118 0l232 88A8 8 0 0 1 496 128zm-24 304H40c-13.255 0-24 10.745-24 24v16a8 8 0 0 0 8 8h464a8 8 0 0 0 8-8v-16c0-13.255-10.745-24-24-24zM96 192v192H60c-6.627 0-12 5.373-12 12v20h416v-20c0-6.627-5.373-12-12-12h-36V192h-64v192h-64V192h-64v192h-64V192H96z"
-                className="fa-secondary"
-                ></path>
-                <path
+                  <path
+                    fill="currentColor"
+                    d="M496 128v16a8 8 0 0 1-8 8h-24v12c0 6.627-5.373 12-12 12H60c-6.627 0-12-5.373-12-12v-12H24a8 8 0 0 1-8-8v-16a8 8 0 0 1 4.941-7.392l232-88a7.996 7.996 0 0 1 6.118 0l232 88A8 8 0 0 1 496 128zm-24 304H40c-13.255 0-24 10.745-24 24v16a8 8 0 0 0 8 8h464a8 8 0 0 0 8-8v-16c0-13.255-10.745-24-24-24zM96 192v192H60c-6.627 0-12 5.373-12 12v20h416v-20c0-6.627-5.373-12-12-12h-36V192h-64v192h-64V192h-64v192h-64V192H96z"
+                    className="fa-secondary"
+                  ></path>
+                  <path
                     fill="currentColor"
                     d="M256,320c-90.52344,0-174.80469-13.23047-246.27344-35.73438a246.11376,246.11376,0,0,0,6.98438,35.998C87.28906,340.53906,168.65625,352,256,352s168.71094-11.46094,239.28906-31.73633a246.11376,246.11376,0,0,0,6.98438-35.998C430.80469,306.76953,346.52344,320,256,320Zm-64-80a80,80,0,1,0-80-80A80.00021,80.00021,0,0,0,192,240Zm0-104a24,24,0,1,1-24,24A23.99993,23.99993,0,0,1,192,136Z"
                     className="fa-primary"
-                ></path>
-
-
+                  ></path>
                 </g>
-                </svg>
-                <span className="link-text">Lenders</span>
+              </svg>
+              <span className="link-text">Lenders</span>
             </a>
-            </li>
-    
-            <li className="nav-item">
+          </li>
+
+          <li className="nav-item">
             <a href="/contracts" className="nav-link">
-                <svg
+              <svg
                 aria-hidden="true"
                 focusable="false"
                 data-prefix="fas"
@@ -127,22 +127,22 @@ function Navbar() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 576 512"
                 className="svg-inline--fa fa-file-signature fa-w-18"
-                >
+              >
                 <g className="fa-group">
-                <path
-                fill="currentColor"
-                d="M218.17 424.14c-2.95-5.92-8.09-6.52-10.17-6.52s-7.22.59-10.02 6.19l-7.67 15.34c-6.37 12.78-25.03 11.37-29.48-2.09L144 386.59l-10.61 31.88c-5.89 17.66-22.38 29.53-41 29.53H80c-8.84 0-16-7.16-16-16s7.16-16 16-16h12.39c4.83 0 9.11-3.08 10.64-7.66l18.19-54.64c3.3-9.81 12.44-16.41 22.78-16.41s19.48 6.59 22.77 16.41l13.88 41.64c19.75-16.19 54.06-9.7 66 14.16 1.89 3.78 5.49 5.95 9.36 6.26v-82.12l128-127.09V160H248c-13.2 0-24-10.8-24-24V0H24C10.7 0 0 10.7 0 24v464c0 13.3 10.7 24 24 24h336c13.3 0 24-10.7 24-24v-40l-128-.11c-16.12-.31-30.58-9.28-37.83-23.75zM384 121.9c0-6.3-2.5-12.4-7-16.9L279.1 7c-4.5-4.5-10.6-7-17-7H256v128h128v-6.1zm-96 225.06V416h68.99l161.68-162.78-67.88-67.88L288 346.96zm280.54-179.63l-31.87-31.87c-9.94-9.94-26.07-9.94-36.01 0l-27.25 27.25 67.88 67.88 27.25-27.25c9.95-9.94 9.95-26.07 0-36.01z"
-                className="fa-secondary"
-                ></path>
+                  <path
+                    fill="currentColor"
+                    d="M218.17 424.14c-2.95-5.92-8.09-6.52-10.17-6.52s-7.22.59-10.02 6.19l-7.67 15.34c-6.37 12.78-25.03 11.37-29.48-2.09L144 386.59l-10.61 31.88c-5.89 17.66-22.38 29.53-41 29.53H80c-8.84 0-16-7.16-16-16s7.16-16 16-16h12.39c4.83 0 9.11-3.08 10.64-7.66l18.19-54.64c3.3-9.81 12.44-16.41 22.78-16.41s19.48 6.59 22.77 16.41l13.88 41.64c19.75-16.19 54.06-9.7 66 14.16 1.89 3.78 5.49 5.95 9.36 6.26v-82.12l128-127.09V160H248c-13.2 0-24-10.8-24-24V0H24C10.7 0 0 10.7 0 24v464c0 13.3 10.7 24 24 24h336c13.3 0 24-10.7 24-24v-40l-128-.11c-16.12-.31-30.58-9.28-37.83-23.75zM384 121.9c0-6.3-2.5-12.4-7-16.9L279.1 7c-4.5-4.5-10.6-7-17-7H256v128h128v-6.1zm-96 225.06V416h68.99l161.68-162.78-67.88-67.88L288 346.96zm280.54-179.63l-31.87-31.87c-9.94-9.94-26.07-9.94-36.01 0l-27.25 27.25 67.88 67.88 27.25-27.25c9.95-9.94 9.95-26.07 0-36.01z"
+                    className="fa-secondary"
+                  ></path>
                 </g>
-                </svg>
-                <span className="link-text">Contracts</span>
+              </svg>
+              <span className="link-text">Contracts</span>
             </a>
-            </li>
-   
-            <li className="nav-item" id="themeButton">
+          </li>
+
+          <li className="nav-item" id="themeButton">
             <a href="/" className="nav-link">
-                <svg
+              <svg
                 aria-hidden="true"
                 focusable="false"
                 data-prefix="fas"
@@ -151,31 +151,29 @@ function Navbar() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
                 className="svg-inline--fa fa-phone-alt fa-w-16"
-                >
+              >
                 <g className="fa-group">
-                <path
-                fill="currentColor"
-                d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z"
-                className="fa-secondary"
-                ></path>
+                  <path
+                    fill="currentColor"
+                    d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z"
+                    className="fa-secondary"
+                  ></path>
                 </g>
-                </svg>
-                <span className="link-text">Contact Us</span>
+              </svg>
+              <span className="link-text">Contact Us</span>
             </a>
-            </li>
+          </li>
         </ul>
-        </nav>
-        </>
-    );
+      </nav>
+    </>
+  );
 }
-
-
-
 
 export default Navbar;
 
 // This is the code for a Moon icon
-                {/* <svg
+{
+  /* <svg
                 className="theme-icon"
                 id="lightIcon"
                 aria-hidden="true"
@@ -199,4 +197,5 @@ export default Navbar;
                     className="fa-primary"
                     ></path>
                 </g>
-                </svg> */}
+                </svg> */
+}
