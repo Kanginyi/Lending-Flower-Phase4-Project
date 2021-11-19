c506086661503c2f001390e779484e6ca27a4c3d
Eric Yi <erickanginyi@gmail.com>
2021-11-19 02:29:21 -0500

    Remove old form, update new

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -3,62 +3,6 @@ import React from 'react';
 function Contact() {
     return (
         <>
-        <section>
-            <div className="container">
-                <h2>Contact Us</h2>
-                <div className="row100">
-                    <div className="col">
-                        <div className="inputBox">
-                            <input type="text" name="" required="required" />
-                                <span className="text">First Name</span>
-                                <span className="line"></span>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="inputBox">
-                            <input type="text" name="" required="required" />
-                                <span className="text">Last Name</span>
-                                    <span className="line"></span>
-                        </div>
-                    </div>
-                </div>
-                <div className="row100">
-                    <div className="col">
-                        <div className="inputBox">
-                            <input type="text" name="" required="required" />
-                                <span className="text">Email</span>
-                                <span className="line"></span>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="inputBox">
-                            <input type="text" name="" required="required" />
-                                <span className="text">Mobile</span>
-                                <span className="line"></span>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="row100">
-                    <div className="col">
-                        <div className="inputBox textarea">
-                            <textarea style={{resize: "none"}} required="required"></textarea>
-                            <span className="text">Type Your Message Here</span>
-                            <span className="line"></span>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="row100">
-                    <div className="col">
-                        <input type="submit" value="send"></input>
-                    </div>
-                </div>
-            </div>
-            
-        </section>
-
-
             <div>
                 <h2>Kevin Lutz</h2>
                 <ul>
