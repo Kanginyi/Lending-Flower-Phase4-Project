812ae4986471834d14c22f6a0470cf5619afe17a
Eric Yi <erickanginyi@gmail.com>
2021-11-19 07:23:17 -0500

    Update a link colors

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -4,12 +4,12 @@ function Contact() {
     return (
         <div id="contact-page-div">
             <div className="contact-div" id="kevin-lutz">
-                <h2>Kevin Lutz</h2>
                 <ul>
                     <a href="https://github.com/kevinlutz" target="_blank"  className="contact-links">Github</a><br/>
                     <a href="https://www.kevinlutz.com/" target="_blank"  className="contact-links">Blog</a><br/>
                     <a href="https://www.youtube.com/channel/UCf2APcfTeFYTu1Gd777viFw" target="_blank"  className="contact-links">YouTube</a><br/>
                 </ul>
+                <h2>Kevin Lutz</h2>
             </div>
             <div className="contact-div" id="eric-yi">
                 <h2>Eric Yi</h2>
