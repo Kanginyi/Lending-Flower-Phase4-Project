d929636254dc2b523a568aba851a496bb1e47601
Eric Yi <erickanginyi@gmail.com>
2021-11-19 10:13:50 -0500

    Update description

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -13,16 +13,19 @@ function Home() {
         muted
         autoPlay={true}
       />
-      <div id="homepage-paras-div">
-        <p className="homepage-paras">
-          Lending Breeze is a lending platform where you can compare rates from
-          accredited lenders and find the best deals for your loans.
-        </p>
-        <p className="homepage-paras">
-          Whether you want to buy a house or want to start a small business to sell your famous chili (don't drop it), we've got you
-          covered. Lending Breeze really makes it a breeze! We're the only online
-          loan marketplace that is fake af but also looks legit af.
-        </p>
+      <div id="homepage-paras-parent">
+        <div id="homepage-paras-div">
+          <div className="homepage-para-divs">
+            <p className="homepage-paras">
+              Lending Breeze is a lending platform where you can compare rates from accredited lenders and find the best deals for your loans.
+            </p>
+          </div>
+          <div className="homepage-para-divs">
+            <p className="homepage-paras">
+              Whether you want to buy a house or start a small business to sell your famous chili (don't drop it), we've got you covered. Lending Breeze really makes it a breeze! We're the only online loan marketplace that isn't actually a loan marketplace!
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
