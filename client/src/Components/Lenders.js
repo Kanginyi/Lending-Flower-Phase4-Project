ca1331130689cfeaa05ecc9fa800a72636f9c6a8
Eric Yi <erickanginyi@gmail.com>
2021-11-18 22:08:18 -0500

    Add images and styling to cards

--- client/src/Components/Lenders.js
+++ client/src/Components/Lenders.js
@@ -22,28 +22,28 @@ function Lenders() {
 
   return (
     <div>
-      <div className="lender-card">
-        <h3>{jpMorgan?.name}</h3>
+      <div className="lender-card" id="jpmorgan-card">
+        <h3><span><img src="https://nhqux1e4gny1xohv8r1ltd1s-wpengine.netdna-ssl.com/wp-content/uploads/2020/12/JPMorganChase.Logo_-600x600.png" /></span> {jpMorgan?.name}</h3>
         <h4>Current Interest Rate: {jpMorgan?.interest_rate}%</h4>
         <p>Description: Interest Only For First Three Months</p>
       </div>
-      <div className="lender-card">
-        <h3>{canadia?.name}</h3>
+      <div className="lender-card" id="canadia-card">
+        <h3><span><img src="https://logodix.com/logo/295115.png" /></span>{canadia?.name}</h3>
         <h4>Current Interest Rate: {canadia?.interest_rate}%</h4>
         <p>Description: Debt Consolidation</p>
       </div>
-      <div className="lender-card">
-        <h3>{capitalTwo?.name}</h3>
+      <div className="lender-card" id="capitaltwo-card">
+        <h3><span><img src="https://logodix.com/logo/516099.png" /></span>{capitalTwo?.name}</h3>
         <h4>Current Interest Rate: {capitalTwo?.interest_rate}%</h4>
         <p>Description: Fixed Rate Mortgage</p>
       </div>
-      <div className="lender-card">
-        <h3>{silverman?.name}</h3>
+      <div className="lender-card" id="silverman-card">
+        <h3><span><img src="https://logopond.com/logos/fe9d7b3b73404aab22b1adfa84be2391.png" /></span>{silverman?.name}</h3>
         <h4>Current Interest Rate: {silverman?.interest_rate}%</h4>
         <p>Description: Student Loan</p>
       </div>
-      <div className="lender-card">
-        <h3>{ttBank?.name}</h3>
+      <div className="lender-card" id="ttbank-card">
+        <h3><span><img src="" /></span>{ttBank?.name}</h3>
         <h4>Current Interest Rate: {ttBank?.interest_rate}%</h4>
         <p>Description: Auto Loan</p>
       </div>
@@ -52,3 +52,5 @@ function Lenders() {
 }
 
 export default Lenders;
+
+<span><img src="" /></span>
